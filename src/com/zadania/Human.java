87124c44d135e53641c1f4cb39bfb6d07d6a3ed7
dariafabisiak <saperixem@gmail.com>
2022-09-20 23:03:08 +0200

    Zadanie 0 - wstepna konfiguracja

---
+++ src/com/zadania/Human.java
@@ -0,0 +1,4 @@
+package com.zadania;
+
+public class Human {
+}
---
+++ src/com/zadania/Main.java
@@ -0,0 +1,8 @@
+package com.zadania;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
