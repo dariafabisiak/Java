47df3cfe823be46039f470da13459daf4266301e
dariafabisiak <saperixem@gmail.com>
2022-09-20 23:07:37 +0200

    Zadanie 3 i 4 - gey your salary and give yourself a rise

--- src/com/zadania/Human.java
+++ src/com/zadania/Human.java
@@ -1,10 +1,31 @@
 package com.zadania;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Human {
     Animal pet;
     Car car;
 
+    private double salary = 0;
+
     Human()
     {}
 
+    double getSalary() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        System.out.println("Dane zostaly pobrane : " + formatter.format(date));
+        System.out.println("Wartosc twojej wyplaty to: " + salary);
+        return salary;
+    }
+
+    void setSalary(double salary) {
+        if (salary < 0) {
+            return;
+        }
+        System.out.println("Update your profile");
+        System.out.println("Collect personal information from Pani Halinka in building called Kadry");
+        System.out.println("ZUS & US know about your to higher salary than normal people. You should worry about that.");
+        this.salary = salary;
+    }
 }
--- src/com/zadania/Main.java
+++ src/com/zadania/Main.java
@@ -3,9 +3,9 @@ package com.zadania;
 public class Main {
 
     public static void main(String[] args) {
-        Animal pet = new Animal("mysz");
         Human h = new Human();
-        h.pet = pet;
-        h.car = new Car("S" ,"Tesla", 422.0, "Red", 120000.0);
+        h.setSalary(-55);
+
+        h.setSalary(5000);
     }
 }
